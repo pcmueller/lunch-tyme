3c2925f01cedb34aa02d56ea25f8a056295dc8be
pcmueller <pmuellerleile@gmail.com>
2021-10-08 14:07:49 -0600

    Add font and variables

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { Route, Redirect, BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className='app'>
-      <header className='app-header'>
-        App Title
-      </header>
-    </div>
+    <Router>
+      <div className='app'>
+        <header className='app-header'>
+          App Title
+        </header>
+      </div>
+    </Router>
   );
 }
 
