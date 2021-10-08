d1c00d7c213e64e480c484ad8304080280ad7a09
pcmueller <pmuellerleile@gmail.com>
2021-10-08 14:24:00 -0600

    Build List and Card components

---
+++ src/components/Card/Card.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+const Card = () => {
+  return (
+    <article className='card'>
+      RESTAURANT
+    </article>
+  );
+}
+
+export default Card;
---
+++ src/components/List/List.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+const List = () => {
+  return (
+    <section className='list'>
+      LIST ENTRIES
+    </section>
+  );
+}
+
+export default List;
