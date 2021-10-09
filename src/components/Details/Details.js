de7d84fa72886216c41850eb741c38729d39c13c
pcmueller <pmuellerleile@gmail.com>
2021-10-08 21:56:28 -0600

    Create 'buildHTML' helper function

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import MapDisplay from '../MapDisplay/MapDisplay';
 
 const Details = ({ selected, handlePaneClose }) => {
+
+  const [ details, setDetails] = useState();
+
+  useEffect(() => {
+    const buildHTML = () => {
+      return (
+        <div className='details-body'>
+          <p>
+            <span>{selected?.location?.address}</span>
+            <span>
+              {selected?.location?.city}, {selected?.location?.state} {selected?.location?.postalCode}
+            </span>
+          </p>
+          {selected?.contact?.formattedPhone && <p>{selected.contact.formattedPhone}</p>}
+          {selected?.contact?.twitter && <p>@{selected.contact.twitter}</p>}
+        </div>
+      )
+    }
+
+    if (selected) {
+      setDetails(buildHTML);
+    }
+  }, [selected]);
 
   return (
       <section className='details'>
@@ -15,15 +38,7 @@ const Details = ({ selected, handlePaneClose }) => {
           <h2 className='name'>{selected.name}</h2>
           <h3 className='category'>{selected.category}</h3>
         </div>
-        <div className='details-body'>
-          <p>{selected?.location?.address}</p>
-          <p>{selected?.location?.city}, {selected?.location?.state} {selected?.location?.postalCode}</p>
-          <br/>
-          <p>{selected?.contact?.formattedPhone}</p>
-          <br/>
-          <p>@{selected?.contact?.twitter}</p>
-        </div>
-
+        {details}
       </section>
   )
 }
