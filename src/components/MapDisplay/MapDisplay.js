2e82ccda243b781f7a843e5a59f506c18a47b6cb
pcmueller <pmuellerleile@gmail.com>
2021-10-10 13:25:00 -0600

    Refactor MapDisplay callstack

--- src/components/MapDisplay/MapDisplay.js
+++ src/components/MapDisplay/MapDisplay.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
 
 const API_KEY = process.env.REACT_APP_MAPS_API_KEY;
 
 const MapDisplay = ({ google, selected }) => {
+
+  const [ coordinates, setCoordinates] = useState({});
+
+  useEffect(() => {
+    const assignCoordinates = () => {
+
+      setCoordinates({
+        lat: selected?.location?.lat,
+        lng: selected?.location?.lng
+      });
+
+    };
+
+    if (selected?.location) {
+      assignCoordinates();
+    }
+  }, [selected]);
 
   const containerStyles = {
     width: '100%',
@@ -20,24 +37,34 @@ const MapDisplay = ({ google, selected }) => {
   }
 
   return (
-    <Map
-      google={google} 
-      zoom={14}
-      // disableDefaultUI={true}
-      draggable={true}
-      zoomControl={true}
-      mapTypeControl={false}
-      streetViewControl={false}
-      style={mapStyles}
-      containerStyle={containerStyles}
-      zoomControlOptions={zoomStyles}
-      initialCenter={{
-        lat: selected?.location?.lat,
-        lng: selected?.location?.lng
-      }}
-    >
-      <Marker />
-    </Map>
+    <>
+    {coordinates?.lat && 
+      <Map
+        google={google} 
+        zoom={14}
+        // disableDefaultUI={true}
+        draggable={true}
+        zoomControl={true}
+        mapTypeControl={false}
+        streetViewControl={false}
+        style={mapStyles}
+        containerStyle={containerStyles}
+        zoomControlOptions={zoomStyles}
+        initialCenter={coordinates}
+      >
+        <Marker />
+        {/* <InfoWindow
+          visible={showInfoWindow}
+          style={styles.infoWindow}
+        >
+          <div className={classes.infoWindow}>
+            <p>Click on the map or drag the marker to select location where the incident occurred</p>
+          </div>
+        </InfoWindow> */}
+      </Map>
+    }
+    
+    </>
   )
 }
 
